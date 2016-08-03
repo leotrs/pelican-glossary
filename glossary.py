9ab77389cb6c5597c44b74a7b9986fee6aac91ef
leotrs <dleonardotn@gmail.com>
2016-08-03 11:10:31 -0500

    process all articles/pages at the same time, by connecting to generator_finalized instead

--- glossary.py
+++ glossary.py
@@ -4,7 +4,7 @@ and pages, and adds a `definitions` variable visible to all page templates.
 
 """
 
-from pelican import signals, contents
+from pelican import signals
 from bs4 import BeautifulSoup
 
 
@@ -13,30 +13,11 @@ class Definitions():
     exclude = []
 
 
-def extract_definitions(content):
-    soup = BeautifulSoup(content.content, 'html.parser')
-
-    for def_list in soup.find_all('dl'):
-        defns = []
-        for def_title in def_list.find_all('dt'):
-            if def_title.text not in Definitions.exclude:
-                defns.append(
-                    {'title': get_title(def_title),
-                     'link': get_link(def_title, content.url),
-                     'definition': get_def(def_title),
-                     'source': content})
-
-        for defn in defns:
-            defn['see_also'] = [also for also in defns if also is not defn]
-
-        Definitions.definitions += defns
-
-
-def get_title(def_title):
+def make_title(def_title):
     return def_title.text
 
 
-def get_link(def_title, url):
+def make_link(def_title, url):
     a_tag = def_title.findChild('a')
     if a_tag and a_tag['href']:
         return url + a_tag['href']
@@ -44,15 +25,8 @@ def get_link(def_title, url):
         return None
 
 
-def get_def(def_title):
+def make_def(def_title):
     return ''.join(str(t) for t in def_title.find_next('dd').contents)
-
-
-def parse_content(content):
-    if isinstance(content, contents.Static) or not content.content:
-        return
-    else:
-        return extract_definitions(content)
 
 
 def set_definitions(generator, metadata):
@@ -63,7 +37,36 @@ def get_excludes(pelican):
     Definitions.exclude = pelican.settings.get('GLOSSARY_EXCLUDE', [])
 
 
+def parse_all(content_list):
+    for content in content_list:
+        soup = BeautifulSoup(content.content, 'html.parser')
+
+        for def_list in soup.find_all('dl'):
+            defns = []
+            for def_title in def_list.find_all('dt'):
+                if def_title.text not in Definitions.exclude:
+                    defns.append(
+                        {'title': make_title(def_title),
+                         'link': make_link(def_title, content.url),
+                         'definition': make_def(def_title),
+                         'source': content})
+
+            for defn in defns:
+                defn['see_also'] = [d for d in defns if d is not defn]
+
+            Definitions.definitions += defns
+
+
+def parse_articles(generator):
+    return parse_all(generator.articles)
+
+
+def parse_pages(generator):
+    return parse_all(generator.pages)
+
+
 def register():
     signals.initialized.connect(get_excludes)
-    signals.content_object_init.connect(parse_content)
+    signals.article_generator_finalized.connect(parse_articles)
+    signals.page_generator_finalized.connect(parse_pages)
     signals.page_generator_context.connect(set_definitions)
